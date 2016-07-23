615a76f45b82f1a01b5267a7aaad8b04e024f08e
David Lantos <david.lantos@gmail.com>
2016-07-23 01:16:05 +0100

    Reduce repetition in last day of the month generator
    `Date.parse` was used every time a date needed to be set. This has now
    moved up to the top level.

--- spec/generators/last_day_of_month_generator_spec.rb
+++ spec/generators/last_day_of_month_generator_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 
 describe Upcoming::LastDayOfMonthGenerator do
   Given(:subject) { Upcoming::LastDayOfMonthGenerator.new }
-  When(:valid) { subject.valid?(date) }
+  When(:valid) { subject.valid?(Date.parse(date)) }
 
   context 'start of the month is invalid' do
-    Given(:date) { Date.parse('2014-06-01') }
+    Given(:date) { '2014-06-01' }
     Then { !valid }
   end
 
   context 'middle of the month is invalid' do
-    Given(:date) { Date.parse('2014-06-15') }
+    Given(:date) { '2014-06-15' }
     Then { !valid }
   end
 
   context 'last day of the month is valid' do
-    Given(:date) { Date.parse('2014-05-31') }
+    Given(:date) { '2014-05-31' }
     Then { valid }
   end
 
   context 'last day of February in a leap year is valid' do
-    Given(:date) { Date.parse('2012-02-29') }
+    Given(:date) { '2012-02-29' }
     Then { valid }
   end
 end
